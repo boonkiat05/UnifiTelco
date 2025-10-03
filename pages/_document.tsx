c33eb53248f1f9a56883a7c8ba8fd9305f5d52e9
boonkiat05 <boonkiat12152005@gmail.com>
2025-10-03 17:04:31 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en"> {/* 这里设置语言 */}
-      <Head />
+    <Html lang="en">
+      <Head>
+        {/* 这里放你的favicon */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon-192x192.png" sizes="192x192" />
+      </Head>
       <body>
         <Main />
         <NextScript />
