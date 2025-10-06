2459b557f5af3b917cfa5423ee03d850743ac673
boonkiat05 <boonkiat12152005@gmail.com>
2025-10-06 11:58:51 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,10 @@ export default function Document()
 { return ( <Html lang="en"> <Head>
   {/* 这里放你的favicon */} 
   <title>Daftar Unifi Malaysia – Compare & Apply TM Fiber Broadband Plans</title>
+   <meta
+    name="description"
+    content="Compare and apply TM Unifi Malaysia broadband plans online. Fast, easy registration with coverage nationwide."
+  />
   <link rel="canonical" href="https://daftarunifi.com.my/" /> 
   <link rel="icon" href="/favicon.ico" />
   <link rel="icon" href="/favicon-192x192.png" sizes="192x192" /> 
