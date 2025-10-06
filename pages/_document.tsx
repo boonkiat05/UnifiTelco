1da50df5690bb44859a337e0b8df674b52d84988
boonkiat05 <boonkiat12152005@gmail.com>
2025-10-06 11:10:34 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,6 @@ export default function Document() {
       <Head>
         {/* 这里放你的favicon */}
         <title>Daftar Unifi Malaysia – Compare & Apply TM Fiber Broadband Plans</title>
-        <link rel="canonical" href="https://daftarunifi.com.my/" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="icon" href="/favicon-192x192.png" sizes="192x192" />
       </Head>
