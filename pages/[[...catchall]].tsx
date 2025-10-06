4aa2a5840863e3a9fde1b57db51c8a922ec928fe
boonkiat05 <boonkiat12152005@gmail.com>
2025-10-06 11:45:41 +0800

    Update [[...catchall]].tsx

--- pages/[[...catchall]].tsx
+++ pages/[[...catchall]].tsx
@@ -1,15 +1,72 @@
-import { Html, Head, Main, NextScript } from 'next/document' 
-export default function Document()
-  { return ( 
-    <Html lang="en"> <Head>
-      {/* 这里放你的favicon */} 
-      <title>Daftar Unifi Malaysia – Compare & Apply TM Fiber Broadband Plans
-    </title> 
-      <link rel="canonical" href="https://daftarunifi.com.my/" /> 
-      <link rel="icon" href="/favicon.ico" /> 
-      <link rel="icon" href="/favicon-192x192.png" sizes="192x192" /> 
-    </Head> 
-      <body> 
-        <Main /> 
-        <NextScript /> </body> </Html> ) 
+import * as React from "react";
+import {
+  PlasmicComponent,
+  extractPlasmicQueryData,
+  ComponentRenderData,
+  PlasmicRootProvider,
+} from "@plasmicapp/loader-nextjs";
+import type { GetStaticPaths, GetStaticProps } from "next";
+
+import Error from "next/error";
+import { useRouter } from "next/router";
+import { PLASMIC } from "@/plasmic-init";
+
+export default function PlasmicLoaderPage(props: {
+  plasmicData?: ComponentRenderData;
+  queryCache?: Record<string, unknown>;
+}) {
+  const { plasmicData, queryCache } = props;
+  const router = useRouter();
+  if (!plasmicData || plasmicData.entryCompMetas.length === 0) {
+    return <Error statusCode={404} />;
   }
+  const pageMeta = plasmicData.entryCompMetas[0];
+  return (
+    <PlasmicRootProvider
+      loader={PLASMIC}
+      prefetchedData={plasmicData}
+      prefetchedQueryData={queryCache}
+      pageRoute={pageMeta.path}
+      pageParams={pageMeta.params}
+      pageQuery={router.query}
+    >
+      <PlasmicComponent component={pageMeta.displayName} />
+    </PlasmicRootProvider>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { catchall } = context.params ?? {};
+  const plasmicPath = typeof catchall === 'string' ? catchall : Array.isArray(catchall) ? `/${catchall.join('/')}` : '/';
+  const plasmicData = await PLASMIC.maybeFetchComponentData(plasmicPath);
+  if (!plasmicData) {
+    // non-Plasmic catch-all
+    return { props: {} };
+  }
+  const pageMeta = plasmicData.entryCompMetas[0];
+  // Cache the necessary data fetched for the page
+  const queryCache = await extractPlasmicQueryData(
+    <PlasmicRootProvider
+      loader={PLASMIC}
+      prefetchedData={plasmicData}
+      pageRoute={pageMeta.path}
+      pageParams={pageMeta.params}
+    >
+      <PlasmicComponent component={pageMeta.displayName} />
+    </PlasmicRootProvider>
+  );
+  // Use revalidate if you want incremental static regeneration
+  return { props: { plasmicData, queryCache }, revalidate: 60 };
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const pageModules = await PLASMIC.fetchPages();
+  return {
+    paths: pageModules.map((mod) => ({
+      params: {
+        catchall: mod.path.substring(1).split("/"),
+      },
+    })),
+    fallback: "blocking",
+  };
+}里面是这样写的
