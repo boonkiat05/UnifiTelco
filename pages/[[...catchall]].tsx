408b8c79334d3b8e97cc3fbe9c6840056aa90a47
boonkiat05 <boonkiat12152005@gmail.com>
2025-10-06 11:42:51 +0800

    Create [[...catchall]].tsx

---
+++ pages/[[...catchall]].tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document' 
+export default function Document()
+  { return ( 
+    <Html lang="en"> <Head>
+      {/* 这里放你的favicon */} 
+      <title>Daftar Unifi Malaysia – Compare & Apply TM Fiber Broadband Plans
+    </title> 
+      <link rel="canonical" href="https://daftarunifi.com.my/" /> 
+      <link rel="icon" href="/favicon.ico" /> 
+      <link rel="icon" href="/favicon-192x192.png" sizes="192x192" /> 
+    </Head> 
+      <body> 
+        <Main /> 
+        <NextScript /> </body> </Html> ) 
+  }
