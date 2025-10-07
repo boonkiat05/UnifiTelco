4451a9a3da5c002d81219736f1a10b2701866c0b
boonkiat05 <boonkiat12152005@gmail.com>
2025-10-07 09:57:27 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,7 +19,7 @@ export default function Document() {
 
         {/* ✅ 字体优化开始 */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
           rel="stylesheet"
