56399638529981f581262fe5314bdc28043c3461
boonkiat05 <boonkiat12152005@gmail.com>
2025-10-09 14:36:38 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,10 +11,10 @@ export default function Document() {
           name="description"
           content="Compare and apply TM Unifi Malaysia broadband plans online. Fast, easy registration with coverage nationwide."
         />
-        <link rel="icon" href="/favicon.ico" sizes="any" /> <!-- 默认 fallback -->
-        <link rel="icon" type="image/png" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
 
 
